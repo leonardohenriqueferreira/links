611199e405ed196fe4761ac3075af6b63722798d
Leonardo Henrique Ferreira <leo-hf@outlook.com>
2024-04-16 19:25:12 -0300

    fix: fixes avatar alt text

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ function toggleMode() {
   const html = document.documentElement
   const img = document.querySelector("#profile img")
   let mode = ""
+  let alt = "preto"
 
   html.classList.toggle("light")
 
-  if (html.classList.contains("light")) mode = "-light"
+  if (html.classList.contains("light")) {
+    mode = "-light"
+    alt = "branco"
+  }
 
   img.setAttribute("src", `./assets/avatar${mode}.png`)
+  img.setAttribute(
+    "alt",
+    `Foto do Leonardo Henrique Ferreira sorrindo, de camisa azul e fundo ${alt}.`
+  )
 }
